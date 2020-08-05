ea26d129938f154294d7f7d18151b5e3780fe310
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-08-05 19:35:29 -0400

    Listing classes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,6 +6,7 @@ const routes = express.Router()
 
 const classesController = new ClassesController()
 
+routes.get('/classes', classesController.index)
 routes.post('/classes', classesController.create)
 
 export default routes
