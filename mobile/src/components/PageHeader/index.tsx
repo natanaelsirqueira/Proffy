1017230af87e58813b624a25a8640d01d3cb2cd8
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-08-08 15:30:38 -0400

    App layout

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+import { BorderlessButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+
+import logoImg from '../../assets/images/logo.png'
+import backIcon from '../../assets/images/icons/back.png'
+
+import styles from './styles'
+
+interface PageHeaderProps {
+  title: string
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
+  const { navigate } = useNavigation()
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={() => navigate('Landing')}>
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+
+      <Text style={styles.title}>
+        {props.title}
+      </Text>
+    </View>
+  )
+}
+
+export default PageHeader
